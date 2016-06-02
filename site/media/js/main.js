671e22f58d48ac720368b053cbf18a7cfd84df24
Alyssa Saucedo <asilla7@gmail.com>
2016-06-02 15:12:03 -0700

    VR exploration
    using the style guide as a reference, just creating vr ideas

--- site/media/js/main.js
+++ site/media/js/main.js
@@ -1,5 +1,67 @@
 jQuery(function($) {
 
+    // var imageSwap = $('.vr-container').find('.image-swap');
+    // var video = $('.vr-container').find('.video');
+    // var graph = $('.vr-container').find('.graph');
+    // var moreInfo = $('.vr-container').find('.more-info');
+
+    // $('.btn-video').on('click', function(){
+    //     imageSwap.removeClass('active').addClass('inactive');
+    //     video.addClass('active');
+    // });
+
+    // $('.btn-graph').on('click', function(){
+    //     imageSwap.removeClass('active').addClass('inactive');
+    //     video.addClass('active');
+    // });
+
+    // $('.btn-more').on('click', function(){
+    //     imageSwap.removeClass('active').addClass('inactive');
+    //     video.addClass('active');
+    // });
+
+    // $('.btn-image').on('click', function(){
+    //     imageSwap.removeClass('active').addClass('inactive');
+    //     video.addClass('active');
+    // });
+
+      $('ul.tabs').each(function(){
+        // For each set of tabs, we want to keep track of
+        // which tab is active and its associated content
+        var $active, $content, $links = $(this).find('a');
+
+        // If the location.hash matches one of the links, use that as the active tab.
+        // If no match is found, use the first link as the initial active tab.
+        $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+        $active.addClass('active');
+
+        $content = $($active[0].hash);
+
+        // Hide the remaining content
+        $links.not($active).each(function () {
+          $(this.hash).hide();
+        });
+
+        // Bind the click event handler
+        $(this).on('click', 'a', function(e){
+          // Make the old tab inactive.
+          $active.removeClass('active');
+          $content.hide();
+
+          // Update the variables with the new link and content
+          $active = $(this);
+          $content = $(this.hash);
+
+          // Make the tab active.
+          $active.addClass('active');
+          $content.show();
+
+          // Prevent the anchor's default click action
+          e.preventDefault();
+        });
+      });
+
+
     var $window = $(window);
     var $input = $('.wrapper input');
 
